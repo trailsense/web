8c88f15c02791e44b629a37e613158f71b81d97a
lea <etzlinger.lea@gmail.com>
2026-04-13 15:13:12 +0200

    fix: Node selection on Trails Tap View

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,13 +1,13 @@
 <template>
   <NuxtLayout name="default">
-    <template #sidebar="{ viewMode }">
+    <template #sidebar>
       <template v-if="viewMode === 'nodes'">
         <NodeSidebarList
           v-if="!selectedNode"
           :error-text="nodesErrorText"
           :is-loading="isNodesLoading"
           :nodes="nodes"
-          @select="selectNode"
+          @select="selectNodeHandler"
           @hover="hoveredNodeId = $event"
           @leave="hoveredNodeId = null"
         />
@@ -15,7 +15,7 @@
         <NodeSidebarDetails
           v-else
           :node="selectedNode"
-          @back="selectNode(null)"
+          @back="backFromNode"
         />
       </template>
 
@@ -23,7 +23,7 @@
         <TrailSidebarList
           v-if="!selectedTrail"
           :trails="trails"
-          @select="selectTrail"
+          @select="selectTrailHandler"
           @hover="hoveredTrailId = $event"
           @leave="hoveredTrailId = null"
         />
@@ -31,7 +31,7 @@
         <TrailSidebarDetails
           v-else
           :trail="selectedTrail"
-          @back="selectTrail(null)"
+          @back="backFromTrail"
         />
       </template>
     </template>
@@ -44,10 +44,10 @@
       :hovered-node-id="hoveredNodeId"
       :selected-trail-id="selectedTrailId"
       :hovered-trail-id-from-list="hoveredTrailId"
-      @select-node="selectNode"
+      @select-node="selectNodeHandler"
       @hover-node="hoveredNodeId = $event"
       @leave-node="hoveredNodeId = null"
-      @select-trail="selectTrail"
+      @select-trail="selectTrailHandler"
     />
   </NuxtLayout>
 </template>
@@ -84,7 +84,23 @@ const selectedTrail = computed(() =>
   trails.value.find(t => t.id === selectedTrailId.value) ?? null
 )
 
-const selectTrail = (id: string | null) => {
+const viewMode = useState<'nodes' | 'trails'>('dashboard:viewMode')
+
+const selectNodeHandler = (id: string | null) => {
+  selectNode(id)
+  viewMode.value = 'nodes'
+}
+
+const selectTrailHandler = (id: string | null) => {
   selectedTrailId.value = id
+  viewMode.value = 'trails'
+}
+
+const backFromNode = () => {
+  selectNode(null)
+}
+
+const backFromTrail = () => {
+  selectedTrailId.value = null
 }
 </script>
